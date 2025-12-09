eccf7182c95bd32aba67e2c392f3c38ff5a78309
sev <severin.schmidmeier@tum.de>
2025-12-09 16:15:22 +0100

    added day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,37 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.runningFold(50) {
+            acc, s ->
+                when(s.first()) {
+                    'R' -> (acc + s.drop(1).toInt()).mod(100)
+                    'L' -> (acc - s.drop(1).toInt()).mod(100)
+                    else -> throw UnsupportedOperationException()
+                }
+        }.count {it == 0}
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.fold(Pair(50,0)) {
+                acc, s ->
+            when(s.first()) {
+                'R' -> {
+                    val res = (acc.first + s.drop(1).toInt())
+                    Pair(res.mod(100),acc.second + res.div(100))
+                }
+                'L' -> {
+                    val res = (acc.first - s.drop(1).toInt())
+                    // extra if we land on 0 or move left past zero. no extra if we start at zero and move left
+                    val extra = if (res == 0 || (res < 0 && (acc.first != 0))) 1 else 0
+                    Pair(res.mod(100),acc.second + res.div(100).absoluteValue + extra)
+                }
+                else -> throw UnsupportedOperationException()
+            }
+        }.second
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("day01")
     part1(input).println()
     part2(input).println()
 }
